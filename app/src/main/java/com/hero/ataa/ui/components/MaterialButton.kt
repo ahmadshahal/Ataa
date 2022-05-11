43278e28be7d2ea9b2f012f809d6d01ea0b88a8c
ahmadshahal <57716361+ahmadshahal@users.noreply.github.com>
2022-05-11 20:07:28 +0300

    Added MaterialButton.kt

---
+++ app/src/main/java/com/hero/ataa/ui/components/MaterialButton.kt
@@ -0,0 +1,39 @@
+package com.hero.ataa.ui.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun MaterialButton(
+    text: String,
+    onClicked: () -> Unit,
+    backgroundColor: Color,
+    contentColor: Color
+) {
+    Button(
+        onClick = onClicked,
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(47.dp)
+            .clip(RoundedCornerShape(7.dp)),
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = backgroundColor,
+            contentColor = contentColor,
+        )
+    ) {
+        Text(
+            text = text,
+            style = MaterialTheme.typography.button
+        )
+    }
+}
