77ff2fa22c23550b75dbd8de4c44501270408756
ahmadshahal <57716361+ahmadshahal@users.noreply.github.com>
2022-05-11 17:39:17 +0300

    Added NotoSansArabic font

--- app/src/main/java/com/hero/ataa/ui/theme/Type.kt
+++ app/src/main/java/com/hero/ataa/ui/theme/Type.kt
@@ -2,63 +2,71 @@ package com.hero.ataa.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.hero.ataa.R
+
+val NotoSans = FontFamily(
+    Font(R.font.noto_sans_arabic_regular, FontWeight.Normal),
+    Font(R.font.noto_sans_arabic_bold, FontWeight.Bold),
+    Font(R.font.noto_sans_arabic_light, FontWeight.Light)
+)
 
 val Typography = Typography(
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = NotoSans,
         fontWeight = FontWeight.Normal,
         fontSize = 15.sp
     ),
     body2 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = NotoSans,
         fontWeight = FontWeight.Normal,
         fontSize = 14.sp
     ),
     h1 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = NotoSans,
         fontWeight = FontWeight.Bold,
         fontSize = 28.sp
     ),
     h2 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = NotoSans,
         fontWeight = FontWeight.Bold,
         fontSize = 24.sp
     ),
     h3 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = NotoSans,
         fontWeight = FontWeight.Bold,
         fontSize = 17.sp
     ),
     h4 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = NotoSans,
         fontWeight = FontWeight.Bold,
         fontSize = 16.sp
     ),
     h5 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = NotoSans,
         fontWeight = FontWeight.Bold,
         fontSize = 14.sp
     ),
     button = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = NotoSans,
         fontWeight = FontWeight.Normal,
         fontSize = 15.sp
     ),
     caption = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = NotoSans,
         fontWeight = FontWeight.Bold,
         fontSize = 12.sp
     ),
     overline = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = NotoSans,
         fontWeight = FontWeight.Normal,
         fontSize = 13.sp
     ),
     subtitle1 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = NotoSans,
         fontWeight = FontWeight.Normal,
         fontSize = 12.sp
     ),
