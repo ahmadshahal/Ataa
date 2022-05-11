e68fd4ff87093e62d7ca74801063d4da7b06777c
ahmadshahal <57716361+ahmadshahal@users.noreply.github.com>
2022-05-11 16:47:41 +0300

    Added App Colors

--- app/src/main/java/com/hero/ataa/ui/theme/Color.kt
+++ app/src/main/java/com/hero/ataa/ui/theme/Color.kt
@@ -2,7 +2,9 @@ package com.hero.ataa.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Green600 = Color(0xFF3EB489)
+val Black600 = Color(0xFF232323)
+val Gray600 = Color(0xFF696969)
+val Gray300 = Color(0xFFB3B3B3)
+val White300 = Color(0xFFFBFBFB)
+val White800 = Color(0xFFFFFFFF)
--- app/src/main/java/com/hero/ataa/ui/theme/Theme.kt
+++ app/src/main/java/com/hero/ataa/ui/theme/Theme.kt
@@ -1,40 +1,23 @@
 package com.hero.ataa.ui.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
-
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+    primary = Green600,
+    onBackground = Black600,
+    background = White300,
+    secondary = Black600,
+    secondaryVariant = Gray300,
+    surface = Gray600,
+    onPrimary = White800,
+    onSecondary = White800,
 )
 
 @Composable
-fun AtaaTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
-
+fun AtaaTheme(content: @Composable () -> Unit) {
+    val colors = LightColorPalette
     MaterialTheme(
         colors = colors,
         typography = Typography,
