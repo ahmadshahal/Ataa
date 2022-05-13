a2f9011a621b09ca108196413b0baf02ed5c0087
ahmadshahal <57716361+ahmadshahal@users.noreply.github.com>
2022-05-13 09:02:06 +0300

    Forcing Arabic as main Locale.

--- app/src/main/java/com/hero/ataa/MainActivity.kt
+++ app/src/main/java/com/hero/ataa/MainActivity.kt
@@ -3,19 +3,22 @@ package com.hero.ataa
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.hero.ataa.ui.screens.login_screen.LoginScreen
 import com.hero.ataa.ui.theme.AtaaTheme
+import java.util.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Forcing Arabic as main Locale.
+        val config = resources.configuration
+        val locale = Locale("ar")
+        Locale.setDefault(locale)
+        config.setLocale(locale)
+        createConfigurationContext(config)
+        resources.updateConfiguration(config, resources.displayMetrics)
+
         setContent {
             AtaaTheme {
                 LoginScreen()
