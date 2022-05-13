b8c6d928755c2cfe132c33b0e82ab521206f0ece
ahmadshahal <57716361+ahmadshahal@users.noreply.github.com>
2022-05-13 21:29:00 +0300

    Added coroutines, hilt, navigation dependencies

--- app/src/main/java/com/hero/ataa/ui/screens/login_screen/LoginScreen.kt
+++ app/src/main/java/com/hero/ataa/ui/screens/login_screen/LoginScreen.kt
@@ -18,7 +18,6 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import com.hero.ataa.R
 import com.hero.ataa.ui.components.LinedTextField
