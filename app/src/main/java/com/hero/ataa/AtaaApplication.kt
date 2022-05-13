5c7488a4e120a0316d816cba8310b677e4e52030
ahmadshahal <57716361+ahmadshahal@users.noreply.github.com>
2022-05-13 09:03:04 +0300

    Added AtaaApplication.kt

---
+++ app/src/main/java/com/hero/ataa/AtaaApplication.kt
@@ -0,0 +1,14 @@
+package com.hero.ataa
+
+import android.app.Application
+
+class AtaaApplication : Application() {
+    /*
+    override fun attachBaseContext(base: Context) {
+        val config = Configuration()
+        val locale = Locale("ar")
+        config.setLocales(LocaleList(locale))
+        super.attachBaseContext(base.createConfigurationContext(config))
+    }
+    */
+}
