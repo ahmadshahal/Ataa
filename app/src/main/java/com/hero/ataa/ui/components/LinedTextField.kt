9f25c1befab16adf19106dc85dde68a59b94ce2e
ahmadshahal <57716361+ahmadshahal@users.noreply.github.com>
2022-05-14 08:06:25 +0300

    Updated LinedTextField.kt

--- app/src/main/java/com/hero/ataa/ui/components/LinedTextField.kt
+++ app/src/main/java/com/hero/ataa/ui/components/LinedTextField.kt
@@ -1,5 +1,7 @@
 package com.hero.ataa.ui.components
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.text.KeyboardOptions
@@ -23,34 +25,43 @@ fun LinedTextField(
     trailingIcon: @Composable () -> Unit = {},
     keyboardOptions: KeyboardOptions = KeyboardOptions(),
     visualTransformation: VisualTransformation = VisualTransformation.None,
-    isError: Boolean = false
+    isError: Boolean = false,
+    errorMessage: String = "",
 ) {
-    TextField(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(53.dp),
-        value = value,
-        onValueChange = onValueChanged,
-        colors = TextFieldDefaults.textFieldColors(
-            backgroundColor = Color.Transparent,
-            errorCursorColor = MaterialTheme.colors.error,
-            errorLabelColor = MaterialTheme.colors.error,
-            errorIndicatorColor = MaterialTheme.colors.error,
-            errorLeadingIconColor = MaterialTheme.colors.secondaryVariant,
-            errorTrailingIconColor = MaterialTheme.colors.secondaryVariant,
-        ),
-        isError = isError,
-        textStyle = MaterialTheme.typography.body2.copy(color = MaterialTheme.colors.onBackground),
-        leadingIcon = leadingIcon,
-        trailingIcon = trailingIcon,
-        placeholder = {
+    Column {
+        TextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(53.dp),
+            value = value,
+            onValueChange = onValueChanged,
+            colors = TextFieldDefaults.textFieldColors(
+                backgroundColor = Color.Transparent,
+                errorCursorColor = MaterialTheme.colors.primary,
+                errorIndicatorColor = MaterialTheme.colors.error,
+                errorLeadingIconColor = MaterialTheme.colors.primaryVariant,
+                errorTrailingIconColor = MaterialTheme.colors.primaryVariant,
+            ),
+            isError = isError,
+            textStyle = MaterialTheme.typography.body2.copy(color = MaterialTheme.colors.onBackground),
+            leadingIcon = leadingIcon,
+            trailingIcon = trailingIcon,
+            placeholder = {
+                Text(
+                    text = hint,
+                    style = MaterialTheme.typography.body2.copy(color = MaterialTheme.colors.primaryVariant)
+                )
+            },
+            visualTransformation = visualTransformation,
+            keyboardOptions = keyboardOptions,
+            singleLine = true,
+        )
+        if (isError) {
+            Spacer(modifier = Modifier.height(5.dp))
             Text(
-                text = hint,
-                style = MaterialTheme.typography.body2.copy(color = MaterialTheme.colors.primaryVariant)
+                text = errorMessage,
+                style = MaterialTheme.typography.subtitle1.copy(color = MaterialTheme.colors.error)
             )
-        },
-        visualTransformation = visualTransformation,
-        keyboardOptions = keyboardOptions,
-        singleLine = true,
-    )
+        }
+    }
 }
