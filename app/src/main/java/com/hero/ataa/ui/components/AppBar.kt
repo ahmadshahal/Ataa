9571c3739d546f2cf1e3fdbc0906cabb74cbe621
ahmadshahal <57716361+ahmadshahal@users.noreply.github.com>
2022-05-16 10:05:41 +0300

    Added AppBar.kt

---
+++ app/src/main/java/com/hero/ataa/ui/components/AppBar.kt
@@ -0,0 +1,40 @@
+package com.hero.ataa.ui.components
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun AppBar(
+    title: @Composable () -> Unit = {},
+    leading: @Composable () -> Unit = {},
+    actions: @Composable RowScope.() -> Unit = {},
+) {
+    TopAppBar(
+        backgroundColor = MaterialTheme.colors.background,
+        elevation = 0.dp,
+        contentColor = MaterialTheme.colors.onBackground,
+    ) {
+        Box(modifier = Modifier.fillMaxSize()) {
+            Box(modifier = Modifier.align(Alignment.Center)) {
+                title()
+            }
+            Box(modifier = Modifier.align(Alignment.CenterStart)) {
+                leading()
+            }
+            Box(modifier = Modifier.align(Alignment.CenterEnd)) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.End
+                ) {
+                    actions()
+                }
+            }
+        }
+    }
+}
