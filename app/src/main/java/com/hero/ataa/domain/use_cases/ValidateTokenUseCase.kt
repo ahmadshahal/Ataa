f442919ce61f21a2c0ed958690e023dd1c730333
ahmadshahal <57716361+ahmadshahal@users.noreply.github.com>
2022-08-11 08:06:12 +0300

    Fixed ValidateTokenUseCase.kt no internet issue

--- app/src/main/java/com/hero/ataa/domain/use_cases/ValidateTokenUseCase.kt
+++ app/src/main/java/com/hero/ataa/domain/use_cases/ValidateTokenUseCase.kt
@@ -1,49 +1,49 @@
 package com.hero.ataa.domain.use_cases
 
-import android.content.Context
 import com.hero.ataa.R
 import com.hero.ataa.data.local.repositories.UserRepository
 import com.hero.ataa.data.remote.repositories.AuthRepository
 import com.hero.ataa.shared.AtaaException
 import com.hero.ataa.shared.DataState
 import com.hero.ataa.shared.UiText
-import com.hero.ataa.system.hasNetwork
-import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.flow
+import java.net.ConnectException
+import java.net.SocketTimeoutException
+import java.net.UnknownHostException
 import javax.inject.Inject
 
 class ValidateTokenUseCase @Inject constructor(
     private val userRepository: UserRepository,
     private val authRepository: AuthRepository,
-    @ApplicationContext private val context: Context,
 ) {
     operator fun invoke() = flow<DataState<Nothing>> {
         emit(DataState.Loading())
         try {
-//            delay(3000)
             val token = userRepository.user().token
             if (token.isNotEmpty()) {
-                if (context.hasNetwork()) {
-                    authRepository.validateToken(token)
-                    userRepository.triggerLoggedInValue(true)
-                } else {
-                    userRepository.triggerLoggedInValue(true)
-                }
+                authRepository.validateToken(token)
+                userRepository.triggerLoggedInValue(true)
             } else {
                 userRepository.triggerLoggedInValue(false)
             }
             emit(DataState.SuccessWithoutData())
+        } catch (ex: UnknownHostException) {
+            userRepository.triggerLoggedInValue(true)
+            emit(DataState.Error(UiText.ResourceText(R.string.no_internet_connection)))
+        } catch (ex: ConnectException) {
+            userRepository.triggerLoggedInValue(true)
+            emit(DataState.Error(UiText.ResourceText(R.string.no_internet_connection)))
+        } catch (ex: SocketTimeoutException) {
+            userRepository.triggerLoggedInValue(true)
+            emit(DataState.Error(UiText.ResourceText(R.string.no_internet_connection)))
         } catch (ex: AtaaException) {
+            userRepository.triggerLoggedInValue(false)
             userRepository.update {
                 it.copy(name = "", email = "", token = "")
             }
-            userRepository.triggerLoggedInValue(false)
             emit(DataState.Error(UiText.DynamicText(ex.message)))
         } catch (ex: Exception) {
-            userRepository.update {
-                it.copy(name = "", email = "", token = "")
-            }
-            userRepository.triggerLoggedInValue(false)
+            userRepository.triggerLoggedInValue(true)
             emit(
                 DataState.Error(
                     if (ex.message != null)
