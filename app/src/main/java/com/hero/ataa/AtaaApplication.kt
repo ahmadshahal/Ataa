643e5cd843fa5beacc1de75db835ccde1b6ce240
ahmadshahal <57716361+ahmadshahal@users.noreply.github.com>
2022-08-12 16:09:27 +0300

    Tiny Change

--- app/src/main/java/com/hero/ataa/AtaaApplication.kt
+++ app/src/main/java/com/hero/ataa/AtaaApplication.kt
@@ -1,6 +1,9 @@
 package com.hero.ataa
 
 import android.app.Application
+import androidx.core.app.NotificationManagerCompat
+import com.hero.ataa.shared.Constants
+import com.hero.ataa.system.notification.NotificationHelper
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
@@ -13,4 +16,14 @@ class AtaaApplication : Application() {
         super.attachBaseContext(base.createConfigurationContext(config))
     }
     */
+    override fun onCreate() {
+        super.onCreate()
+        NotificationHelper.createNotificationChannel(
+            context = this,
+            name = Constants.NOTIFICATION_CHANNEL_NAME,
+            description = getString(R.string.ataa_notification_channel),
+            importance = NotificationManagerCompat.IMPORTANCE_DEFAULT,
+            showBadge = false,
+        )
+    }
 }
--- app/src/main/java/com/hero/ataa/system/notification/NotificationsRepository.kt
+++ app/src/main/java/com/hero/ataa/system/notification/NotificationsRepository.kt
@@ -1,7 +1,6 @@
 package com.hero.ataa.system.notification
 
 import android.content.Context
-import androidx.core.app.NotificationManagerCompat
 import com.hero.ataa.R
 import com.hero.ataa.shared.Constants
 import dagger.hilt.android.qualifiers.ApplicationContext
@@ -11,19 +10,12 @@ class NotificationsRepository @Inject constructor(
     @ApplicationContext private val context: Context,
 ) {
     fun triggerPaymentNotification() {
-        NotificationHelper.createNotificationChannel(
-            context = context,
-            name = Constants.NOTIFICATION_CHANNEL_NAME,
-            description = context.getString(R.string.ataa_notification_channel),
-            importance = NotificationManagerCompat.IMPORTANCE_DEFAULT,
-            showBadge = false,
-        )
         NotificationHelper.createNotification(
             context,
-            title = context.getString(R.string.donate_now),
-            content = context.getString(R.string.donate_to_ataa_charity_project),
+            title = context.getString(R.string.thank_you),
+            content = context.getString(R.string.thank_you_for_considering),
             channelName = Constants.NOTIFICATION_CHANNEL_NAME,
-            bigText = context.getString(R.string.donate_to_ataa_charity_project),
+            bigText = context.getString(R.string.thank_you_for_considering),
         )
     }
 }
